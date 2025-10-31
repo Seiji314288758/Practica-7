adb3d73f592bb3dee64414a7a7f00b602236a2fd
Seiji314288758 <nietoseiji@gmail.com>
2025-10-31 13:46:26 -0600

    Create Ejercicio1.c

---
+++ Ejercicio1.c
@@ -0,0 +1,16 @@
+/*Victor Seiji Nieto Morita
+practica 7 ejercicio 1*/
+
+#include<stdio.h>
+int main ()
+{
+int a, b;
+a = 3;
+b = 2;
+if (a > b)
+{
+printf("\ta (%d) es mayor a b (%d).\n",a,b);
+}
+printf("\tEl programa sigue su flujo.\n");
+return 0;
+}
